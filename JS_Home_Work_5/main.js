809369af18a054ba60cc6ff0680cfbd18f6dc194
OlegHellBasS <oleghellbass1@gmail.com>
2023-05-17 00:29:56 +0300

    JS_Home_Work_5 Finish

--- JS_Home_Work_5/main.js
+++ JS_Home_Work_5/main.js
@@ -1,5 +1,5 @@
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-const rectangle = (a,b) => a * 2 + b * 2;
+const rectangle = (a, b) => a * 2 + b * 2;
 console.log(rectangle(10, 5));
 //---------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
@@ -7,8 +7,8 @@ const circle = r => Math.PI * r * r;
 console.log(circle(5));
 //---------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-const cylinder = (r,h) => 2 * Math.PI * r * (r + h);
-console.log(cylinder(5,4));
+const cylinder = (r, h) => 2 * Math.PI * r * (r + h);
+console.log(cylinder(5, 4));
 //---------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка приймає масив та виводить кожен його елемент
 const modules = ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java'];
@@ -28,14 +28,14 @@ const createList_3li = text => document.write(`<ul><li>${text}</li><li>${text}</
 createList_3li('Lorem ipsum.')
 //---------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-const createList_countli = (text,count) => {
+const createList_countli = (text, count) => {
     document.write(`<ul>`);
     for (let i = 0; i < count; i++) {
         document.write(`<li>${text}</li>`);
     }
     document.write(`</ul>`);
 }
-createList_countli('Lorem ipsum dolor sit.',5);
+createList_countli('Lorem ipsum dolor sit.', 5);
 //---------------------------------------------------------------------------------------------------------------------
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 const createList_arr = arr => {
@@ -73,11 +73,51 @@ arr_list(users)
 // - створити функцію яка повертає найменьше число з масиву
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const arr_minNumber = arr => {
-
+    let minNumber = arr[0]
+    for (const number of arr) {
+        if (number < minNumber) {
+            minNumber = number
+        }
+    }
+    return minNumber
 }
+let a = arr_minNumber(numbers);
+console.log(a)
 //---------------------------------------------------------------------------------------------------------------------
-// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад sum([1,2,10]) //->13
+let sum = [1, 2, 10];
+const arr_sun = arr => {
+    let total = 0;
+    for (const num of arr) {
+        total += num
+    }
+    return total
+}
+let b = arr_sun(sum)
+console.log(b)
+//---------------------------------------------------------------------------------------------------------------------
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+const swap = [11, 22, 33, 44];
+const arr_swap = (arr, index1, index2) => {
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    console.log(arr);
+}
+arr_swap(swap,0,1)
+//---------------------------------------------------------------------------------------------------------------------
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
+    let result = null;
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            result = sumUAH / currencyValues[i].value;
+            break;
+        }
+    }
+    return result;
+};
+console.log(exchange(10000, [{ currency: 'USD', value: 40 }, { currency: 'EUR', value: 42 }], 'USD'));
